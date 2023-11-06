84fdf78a6b69824ae9fc5a03635d383ef9dd9527
Cortrax <aidenrphotos@gmail.com>
2023-11-06 13:16:26 -0500

    Show fetched data in frontend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     const navigate = useNavigate();
     const [cookies, removeCookie] = useCookies([]);
     const [username, setUsername] = useState('');
-    const postsArr = [];
+    const [postsArr, setPostsArr] = useState([]);
     const titleRef = useRef(null);
     const contentRef = useRef(null);
 
@@ -22,22 +22,30 @@ const Home = () => {
         navigate('/signup');
         localStorage.removeItem('userId');
     };
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const postReq = await axios.post(
+                    'http://localhost:5000/getposts',
+                    {
+                        userId: userId,
+                    }
+                );
 
-    const getPosts = async () => {
-        try {
-            const postReq = await axios.post('http://localhost:5000/getposts', {
-                userId: userId,
-            });
-            const posts = postReq.data.posts;
-            posts.map((post) => {
-                postsArr.push(post);
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    };
+                setPostsArr(postReq.data.posts);
 
-    console.log(postsArr);
+                // posts.map((post, i) => {
+                //     console.log('post', post, i);
+                //     setPostsArr.push(post);
+                // });
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getPosts();
+    }, []);
+
+    console.log('postsArr', postsArr);
 
     const createPost = async () => {
         const title = titleRef.current.value;
@@ -56,14 +64,11 @@ const Home = () => {
         }
     };
 
-    getPosts();
-
-    return (
+    return postsArr.length > 0 ? (
         <Box>
             <Typography sx={{ fontSize: '36px' }}>Home</Typography>
             <MyButton onClick={Logout}>Logout</MyButton>
             <ToastContainer />
-
             <Box
                 component='form'
                 sx={{
@@ -90,12 +95,69 @@ const Home = () => {
                     margin='normal'
                 />
                 <MyButton onClick={createPost}>Create Post</MyButton>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '10px',
+                    }}
+                >
+                    {postsArr.length > 0 &&
+                        postsArr.map((post, i) => (
+                            <Box
+                                key={i}
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    bgcolor: 'primary.grey',
+                                    padding: '20px',
+                                }}
+                            >
+                                <Typography
+                                    key={i + 1}
+                                    sx={{ fontSize: '24px' }}
+                                >
+                                    {post.title}
+                                </Typography>
+                                <Typography key={i + 2}>
+                                    {post.content}
+                                </Typography>
+                            </Box>
+                        ))}
+                </Box>
             </Box>
+        </Box>
+    ) : (
+        <Box>
+            <Typography sx={{ fontSize: '36px' }}>Home</Typography>
+            <MyButton onClick={Logout}>Logout</MyButton>
+            <ToastContainer />
+            <Box
+                component='form'
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography sx={{ fontSize: '24px' }}>Create Post</Typography>
 
-            <Box>
-                {postsArr.map((post) => {
-                    <Typography>{post}</Typography>;
-                })}
+                <TextField
+                    inputRef={titleRef}
+                    sx={textFieldStyling}
+                    label='Title'
+                    variant='outlined'
+                    margin='normal'
+                />
+                <TextField
+                    inputRef={contentRef}
+                    sx={textFieldStyling}
+                    label='Content'
+                    variant='outlined'
+                    margin='normal'
+                />
+                <MyButton onClick={createPost}>Create Post</MyButton>
             </Box>
         </Box>
     );
